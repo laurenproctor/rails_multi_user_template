f2e57219665ec1c9851a2b6e6158459066e6632a
Lauren Proctor <laurenproctor32@gmail.com>
2013-01-26 21:23:41 -0500

    bare bones template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fileshare::Application.config.secret_token = '9957152c2880a72819e5c5bc0e41a239988af46c83849f22640534491aa8b0c6399b253866ae63b82bb31e6c51e7566a357fff0464252e8e95d601ae497d7095'
